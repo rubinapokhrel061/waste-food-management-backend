fb86f62052b78e1d75d9145a92114f065b5ad75b
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2025-10-30 23:03:46 +0545

    update

--- dist/routes/emailRoutes.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-import { sendEmail } from "../controllers/emailController.js";
-const emailRouter = express.Router();
-emailRouter.post("/sendEmail", sendEmail);
-export default emailRouter;
