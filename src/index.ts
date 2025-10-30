8ae26c90c5507d562f6ac05f641cead68ea7c101
Rubina Pokhrel <pokhrelrubina2061@gmail.com>
2025-10-30 22:56:24 +0545

    update cron

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,21 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import emailRouter from "./routes/emailRoutes.js";
 import cors from "cors";
+import cron from "node-cron";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8020;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+cron.schedule("*/10 * * * *", () => {
+  console.log("Task running every 10 minutes");
+});
+
 app.use(express.json());
 
 // Router
@@ -15,7 +24,7 @@ app.use("/api/email", emailRouter);
 
 // Test route
 app.get("/", (_req: Request, res: Response) => {
-  res.send("Hello, TypeScript + SendGrid");
+  res.send("Success");
 });
 
 app.listen(port, () => {
